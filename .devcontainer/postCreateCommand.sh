6dc64210740f0896d9af2b5c30aed8f0d7d3ed97
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2025-10-26 10:29:55 +0900

    devcontainer側も修正　

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 # postCreateCommand.sh
 
+set -e
+
 direnv hook bash >> ~/.bashrc
 direnv allow
 
-pipenv install --dev
-source $(pipenv --venv)/bin/activate
+uv sync --group dev
+if ! grep -q ". .venv/bin/activate" ~/.bashrc 2>/dev/null; then
+  echo ". .venv/bin/activate" >> ~/.bashrc
+fi
